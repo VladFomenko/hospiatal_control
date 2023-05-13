b3e65cb9545e4714394530aa986fe5c504965ec1
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-05-13 19:43:51 +0300

    add doctors_controller.rb

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,49 @@
+class DoctorsController < ApplicationController
+  before_action :doctor_params, only: %i[create update]
+  before_action :set_doctors, only: :index
+  before_action :set_doctor, only: %i[show update destroy]
+
+  def index
+    @doctors
+  end
+
+  def show
+    @doctor
+  end
+
+  def create
+    doctor = Doctor.create(doctor_params)
+
+    if doctor.save
+
+    else
+
+    end
+  end
+
+  def update
+    if @doctor.update(doctor_params)
+
+    else
+
+    end
+  end
+
+  def destroy
+    @doctor.destroy
+  end
+
+  private
+
+  def set_doctors
+    @doctors = Doctor.all
+  end
+
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def doctor_params
+    params.doctor.permit(:first_name, :second_name, :password, :work_experience)
+  end
+end
