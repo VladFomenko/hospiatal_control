31f3e51759f7723e2e4034b886538361d3216946
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-05-13 18:52:43 +0300

    upd seed and database.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# Client.create!(first_name: 'as', second_name: 'asd', phone_number: '+380677777777', password: 'Password123', birth_date: Date.current)
+
