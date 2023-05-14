75d5fd9397d2deb32dc203e8bbf5ab0d9d5160aa
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-05-14 20:48:20 +0300

    add clients devise

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Client < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   include Clientable
 
   has_many :visits, dependent: :destroy
