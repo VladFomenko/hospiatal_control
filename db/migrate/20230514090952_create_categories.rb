2637735dc5c12e9ef0e0476320882afac9be7c82
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-05-20 18:56:49 +0300

    upd migration and db for devise

--- db/migrate/20230514090952_create_categories.rb
+++ db/migrate/20230514090952_create_categories.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class CreateCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :categories do |t|
-      t.string :name
+      t.string :name, null: false
       t.references :doctor, null: false, foreign_key: true
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 client = Client.create!(first_name: 'John', second_name: 'Doe', phone_number: '+380677777777', password: 'Password123',
-               birth_date: Date.new(rand(1991..2005), rand(1..12), rand(1..28)))
+                        birth_date: Date.new(rand(1991..2005), rand(1..12), rand(1..28)))
 doctor = Doctor.create!(first_name: 'Albert', second_name: 'Cody', password: 'Password123', work_experience: 23)
 doctor.categories.create(name: 'Pediatrician')
 client.visits.create(date_of_visit: Date.current + 14, doctor_id: doctor.id)
-
