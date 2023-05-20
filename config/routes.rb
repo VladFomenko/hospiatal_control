c3bcc8048fc26d11e91fef6d44f679b93e577d1a
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-05-20 20:19:53 +0300

    add dashboard and some fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :visits
+    resources :categories
+    resources :clients
+    resources :doctors
+
+    root to: 'clients#index'
+  end
+
   devise_for :doctors
   devise_for :clients
 
