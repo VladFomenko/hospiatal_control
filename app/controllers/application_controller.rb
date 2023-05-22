d9abd34de91855f18b2c478d8006c1c8f13c2fb8
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-05-23 00:15:11 +0300

    create rescue_from and page errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user_based_on_role, except: :home
   before_action :configure_sign_up_params, if: :devise_controller?
 
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from CanCan::AccessDenied, with: :no_permission
+
   def configure_sign_up_params
     if resource_class == Client
       devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name second_name birth_date])
@@ -39,6 +42,14 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def no_permission
+    render 'errors/no_permission', status: :not_found
+  end
+
+  def not_found
+    render 'errors/404', status: :not_found
+  end
 
   def authenticate_user_based_on_role
     if current_client
