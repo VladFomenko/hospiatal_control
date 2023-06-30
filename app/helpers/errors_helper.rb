9a091ee5bd4e8868f39d7f30afcf814cd72986e2
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-06-30 20:26:10 +0300

    add notice password_confirmation

--- app/helpers/errors_helper.rb
+++ app/helpers/errors_helper.rb
@@ -4,4 +4,10 @@ module ErrorsHelper
   def error_attributes(resource)
     resource.errors.map(&:attribute).uniq
   end
+
+  def password_confirmation_error(resource)
+    if error_attributes(resource).include?(:password_confirmation)
+      content_tag(:div, 'Passwords do not match', class: 'alert alert-danger', role: 'alert')
+    end
+  end
 end
